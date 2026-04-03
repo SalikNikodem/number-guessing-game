5733162b6e244e8a5b0d60f9f7b39b87c99c4d71
SalikNikodem <saliknikodem@gmail.com>
2026-04-03 19:29:15 +0200

    helpers

---
+++ helpers.py
@@ -0,0 +1,19 @@
+starting_message = """Welcome to the Number Guessing Game!
+I'm thinking of a number between 1 and 100.
+You have 5 chances to guess the correct number."""
+
+difficulty_select = """Please select the difficulty level:
+1. Easy (10 chances)
+2. Medium (5 chances)
+3. Hard (3 chances)"""
+
+difficulties = {
+    1: "Easy",
+    2: "Medium",
+    3: "Hard"
+}
+chance = {
+    1: 10,
+    2: 5,
+    3: 3
+}
